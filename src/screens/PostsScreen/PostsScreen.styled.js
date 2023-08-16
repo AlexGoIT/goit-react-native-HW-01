5d56c7a9a049a6d1103d18a3eab687b63dbb4f78
AlexGoIT <alexandrpuryaev@gmail.com>
2023-08-16 19:27:56 +0300

    Added posts screen

---
+++ src/screens/PostsScreen/PostsScreen.styled.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  bgImage: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+});
