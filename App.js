05dd6b04421ee90c251b834479b4923d62bb6b3c
AlexGoIT <alexandrpuryaev@gmail.com>
2023-08-20 18:39:14 +0300

    refactoring navigation

--- App.js
+++ App.js
@@ -5,35 +5,30 @@ import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "./src/screens/LoginScreen";
 import RegistrationScreen from "./src/screens/RegistrationScreen";
 
-import PostsScreen from "./src/screens/PostsScreen";
-import Home from "./src/screens/Home/Home";
-import ProfileScreen from "./src/screens/ProfileScreen/ProfileScreen";
-import CommentsScreen from "./src/screens/CommentsScreen";
+// import Home from "./src/screens/Home/Home";
+// import PostsScreen from "./src/screens/PostsScreen";
+// import ProfileScreen from "./src/screens/ProfileScreen";
+// import CommentsScreen from "./src/screens/CommentsScreen";
+
+import AppNavigation from "./src/navigation/AppNavigation";
 
 const MainStack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <MainStack.Navigator initialRouteName="Login">
-        <MainStack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <MainStack.Screen
-          name="Registration"
-          component={RegistrationScreen}
-          options={{ headerShown: false }}
-        />
-        <MainStack.Screen name="Posts" component={PostsScreen} />
-        <MainStack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-        <MainStack.Screen name="Profile" component={ProfileScreen} />
-        <MainStack.Screen name="Comments" component={CommentsScreen} />
+      <MainStack.Navigator
+        initialRouteName="Login"
+        screenOptions={{ headerShown: false }}
+      >
+        <MainStack.Screen name="Login" component={LoginScreen} />
+        <MainStack.Screen name="Registration" component={RegistrationScreen} />
+        <MainStack.Screen name="Home" component={AppNavigation} />
+
+        {/* <MainStack.Screen name="Home" component={Home} /> */}
+        {/* <MainStack.Screen name="Posts" component={PostsScreen} /> */}
+        {/* <MainStack.Screen name="Profile" component={ProfileScreen} /> */}
+        {/* <MainStack.Screen name="Comments" component={CommentsScreen} /> */}
       </MainStack.Navigator>
     </NavigationContainer>
   );
