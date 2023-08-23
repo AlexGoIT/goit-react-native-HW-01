ae32bd107aad1eb0f4116a3bd733acbbcf035ce0
AlexGoIT <alexandrpuryaev@gmail.com>
2023-08-23 14:37:43 +0300

    Added Camera

--- src/screens/CreatePostsScreen/CreatePostScreen.jsx
+++ src/screens/CreatePostsScreen/CreatePostScreen.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import {
   BackgroundView,
   ImageWrapper,
@@ -12,30 +12,132 @@ import {
   MapInput,
   CameraButton,
   DeleteButton,
+  PhotoView,
+  CameraView,
 } from "./CreatePostScreen.styled";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
 
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import { useNavigation } from "@react-navigation/native";
+import { useInputFormReducer } from "../../hooks/useInputFormReducer";
+import { Alert, Text } from "react-native";
+import { TouchableOpacity } from "react-native";
+
 const CreatePostScreen = () => {
+  const navigation = useNavigation();
+
+  const [inputsValue, dispatch] = useReducer(useInputFormReducer, {
+    photoName: "",
+    photoLocation: "",
+  });
+
+  const { photoName, photoLocation } = inputsValue;
+
   const [disabled, setDisabled] = useState(true);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [photo, setPhoto] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      requestPermission();
+      await MediaLibrary.requestPermissionsAsync();
+    })();
+  }, []);
+
+  const handleTakePhoto = async () => {
+    if (cameraRef) {
+      const { uri } = await cameraRef.takePictureAsync();
+      await MediaLibrary.createAssetAsync(uri);
+
+      setPhoto(uri);
+      setDisabled(false);
+    }
+  };
+
+  const handleSendPost = () => {
+    if (!photoName || !photoLocation) {
+      Alert.alert("Заповніть всі поля");
+      return;
+    }
+
+    console.log(inputsValue, photo);
+
+    reset();
+    navigation.navigate("Posts");
+  };
+
+  const reset = () => {
+    dispatch({ type: "photoName", payload: "" });
+    dispatch({ type: "photoLocation", payload: "" });
+
+    setPhoto(null);
+    setDisabled(true);
+  };
 
   return (
     <BackgroundView>
       <ImageWrapper>
-        <CameraButton>
-          <MaterialIcons name="camera-alt" size={24} color="#BDBDBD" />
-        </CameraButton>
+        {permission?.status !== "granted" ? (
+          <>
+            <Text
+              style={{ color: "red", marginBottom: 16, textAlign: "center" }}
+            >
+              Потрібно дозволити доступ до камери
+            </Text>
+            <TouchableOpacity onPress={() => requestPermission()}>
+              <Feather name="refresh-cw" size={24} color="#BDBDBD" />
+            </TouchableOpacity>
+          </>
+        ) : photo ? (
+          <PhotoView src={photo} />
+        ) : (
+          <Camera
+            type={type}
+            ref={setCameraRef}
+            style={{
+              width: "100%",
+              height: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <CameraView>
+              <CameraButton onPress={handleTakePhoto}>
+                <MaterialIcons name="camera-alt" size={24} color="#BDBDBD" />
+              </CameraButton>
+            </CameraView>
+          </Camera>
+        )}
       </ImageWrapper>
       <HelpText>Завантажте фото</HelpText>
       <InputWrapper>
-        <Input placeholder="Назва..." />
+        <Input
+          name="photoName"
+          value={photoName}
+          onChangeText={(text) =>
+            dispatch({ type: "photoName", payload: text })
+          }
+          placeholder="Назва..."
+        />
         <MapInputWrapper>
-          <MapInput placeholder="Місцевість..." />
+          <MapInput
+            name="photoLocation"
+            value={photoLocation}
+            onChangeText={(text) =>
+              dispatch({ type: "photoLocation", payload: text })
+            }
+            placeholder="Місцевість..."
+          />
           <MapInputIcon name="map-pin" size={24} color="#BDBDBD" />
         </MapInputWrapper>
       </InputWrapper>
       <PostButton
         disabled={disabled}
+        onPress={handleSendPost}
         style={
           disabled
             ? { backgroundColor: "#F6F6F6" }
@@ -48,8 +150,12 @@ const CreatePostScreen = () => {
           Опублікувати
         </PostButtonText>
       </PostButton>
-      <DeleteButton>
-        <Feather name="trash-2" size={24} color="#BDBDBD" />
+      <DeleteButton disabled={disabled} onPress={() => reset()}>
+        <Feather
+          name="trash-2"
+          size={24}
+          color={disabled ? "#BDBDBD" : "#FF6C00"}
+        />
       </DeleteButton>
     </BackgroundView>
   );
